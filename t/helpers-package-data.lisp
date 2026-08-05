6fb0156fa36c4a959c1efc8324e79758d7f8d39b
takeokunn <bararararatty@gmail.com>
2026-08-06 02:15:03 +0900

    fix(test): update +expected-system-metadata+'s version to match the v1.5.0 bump

--- t/helpers-package-data.lisp
+++ t/helpers-package-data.lisp
@@ -7,7 +7,7 @@
     (:homepage . "https://github.com/nerima-lisp/cl-tty-kit")
     (:bug-tracker . "https://github.com/nerima-lisp/cl-tty-kit/issues")
     (:source-control . (:git "https://github.com/nerima-lisp/cl-tty-kit.git"))
-    (:version . "1.4.0")))
+    (:version . "1.5.0")))
 
 (defparameter +expected-external-symbols+ '("CHAR-WIDTH"
     "STRING-WIDTH"
